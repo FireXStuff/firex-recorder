e55574200803229e99304a65d236010a26849a3c
Marc S Delahousse <mdelahou@cisco.com>
2019-05-06 14:44:17 -0400

    Passing in broker and destination file

--- firex_recorder/__main__.py
+++ firex_recorder/__main__.py
@@ -1,4 +1,14 @@
+import argparse
+
 
 def main():
-    print("hello world")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--destination',
+        help='A destination file to contain the recording of celery events')
+    parser.add_argument(
+        "--broker",
+        help="the broker url")
+    args = parser.parse_args()
+    print("hello world form " + args.broker + " to " + args.destination)
 
--- firex_recorder/launcher.py
+++ firex_recorder/launcher.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 
 from celery.utils.log import get_task_logger
+from firexapp.broker_manager.broker_factory import BrokerFactory
 from firexapp.fileregistry import FileRegistry
 from firexapp.submit.uid import Uid
 from firexapp.submit.tracking_service import TrackingService
@@ -11,19 +12,27 @@ from firexapp.submit.tracking_service import TrackingService
 logger = get_task_logger(__name__)
 
 
-RECORDER_LOG_REGISTRY_KEY = 'RECORDER_OUTPUT_LOG_REGISTRY_KEY'
+RECORDER_LOG_REGISTRY_KEY = 'RECORDER_OUTPUT_REGISTRY_KEY'
 FileRegistry().register_file(RECORDER_LOG_REGISTRY_KEY, os.path.join(Uid.debug_dirname, 'recorder.stdout'))
+
+DEFAULT_RECORDER_DEST_REGISTRY_KEY = 'DEFAULT_RECORDER_DEST_REGISTRY_KEY'
+FileRegistry().register_file(DEFAULT_RECORDER_DEST_REGISTRY_KEY, os.path.join(Uid.debug_dirname, 'firex.rec'))
 
 
 class RecorderLauncher(TrackingService):
 
     def extra_cli_arguments(self, arg_parser):
-        # todo: add the ArgParse arguments
-        pass
+        arg_parser.add_argument('--recording', help='A file to record celery events', default=None)
 
     def start(self, args, uid=None, **kwargs)->{}:
         # assemble startup cmd
         cmd = "firex_recorder"
+        if args.recording:
+            cmd += ' --destination ' + args.recording
+        else:
+            cmd += ' --destination ' + FileRegistry().get_file(DEFAULT_RECORDER_DEST_REGISTRY_KEY, uid.logs_dir)
+        cmd += ' --broker ' + BrokerFactory.get_broker_url()
+        cmd += " &"
 
         # start the recording service
         logger.debug("Starting Recorder...")
